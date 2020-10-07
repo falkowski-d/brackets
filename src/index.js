77563f96c508b3e0328f0f7c6fbc1b7f93191166
Denis Falkowski <falkowski.denis@gmail.com>
2020-10-07 21:54:57 +0300

    fix: fix errors

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ module.exports = function check(str, bracketsConfig) {
     let result = [];
     let count = 1;
     let imputStr = str;
-    result = bracketsConfig.map(item => imputStr = imputStr.replace((item[0] + item[1]));
+    result = bracketsConfig.map(item => imputStr = imputStr.replace((item[0] + item[1])));
 
     while (count === 1) {
 
