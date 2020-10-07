f559f70f913356260955241e848112ccc9a15208
Denis Falkowski <falkowski.denis@gmail.com>
2020-10-07 22:24:51 +0300

    fix: fix solution

--- src/index.js
+++ src/index.js
@@ -1,22 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
     let result = [];
-    let count = 1;
+    let count = str.length;
     let imputStr = str;
-    result = bracketsConfig.map(item => imputStr = imputStr.replace((item[0] + item[1])));
+    result = bracketsConfig.map(item => (item[0] + item[1]));
 
-    while (count === 1) {
+    for (let j = 0; j < count; j++){
 
         for (let i = 0; i < result.length; i++) {
             str = str.replace(result[i], '');
         }
-        if (str.length === 0) {
-            count = 0;
+        if (imputStr === str) {
+            j = count;
         }
     }
-    if (str.length === 0 || str !== imputStr) {
+    if (str.length === 0) {
         return true;
     } else return false;
-
 
 
 };
