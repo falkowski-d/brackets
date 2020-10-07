c6f3a3500c6cfd8a9e6082ccb23cc69498cb8654
Denis Falkowski <falkowski.denis@gmail.com>
2020-10-07 21:52:41 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let result = [];
+    let count = 1;
+    let imputStr = str;
+    result = bracketsConfig.map(item => imputStr = imputStr.replace((item[0] + item[1]));
+
+    while (count === 1) {
+
+        for (let i = 0; i < result.length; i++) {
+            str = str.replace(result[i], '');
+        }
+        if (str.length === 0) {
+            count = 0;
+        }
+    }
+    if (str.length === 0 || str !== imputStr) {
+        return true;
+    } else return false;
+
+
+
+};
